72cbb69c279401487f26eaf35254f7242d5f1ae3
yushaojian <yushaojian13@163.com>
2015-12-17 23:40:50 +0800

    lambda & snackbar

--- app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
+++ app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ysj.milkyway.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +17,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.rv_demo)
     RecyclerView demoRecyclerView;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         demoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         demoRecyclerView.setAdapter(new DemoAdapter());
+
+        fab.setOnClickListener(v -> Snackbar.make(v, R.string.app_name, Snackbar.LENGTH_SHORT).show());
     }
 
     private void setupToolbar() {
