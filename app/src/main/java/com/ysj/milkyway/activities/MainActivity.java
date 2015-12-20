940a752a25105e7feee487cecdbe889a3eb39249
yushaojian <yushaojian13@163.com>
2015-12-20 11:30:01 +0800

    floating window created

--- app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
+++ app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
@@ -1,14 +1,15 @@
 package com.ysj.milkyway.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.ysj.milkyway.R;
+import com.ysj.milkyway.services.FloatWindowService;
 import com.ysj.milkyway.views.adapters.DemoAdapter;
 
 import butterknife.Bind;
@@ -31,11 +32,17 @@ public class MainActivity extends AppCompatActivity {
         demoRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         demoRecyclerView.setAdapter(new DemoAdapter());
 
-        fab.setOnClickListener(v -> Snackbar.make(v, R.string.app_name, Snackbar.LENGTH_SHORT).show());
+        fab.setOnClickListener(v -> startHack());
     }
 
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private void startHack() {
+        getApplicationContext().setTheme(R.style.AppTheme);
+        startService(new Intent(this, FloatWindowService.class));
+        finish();
     }
 }
