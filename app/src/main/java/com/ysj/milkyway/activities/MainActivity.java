e44a1cf648956726cf957f76462a2ab220ae0d7f
yushaojian <yushaojian13@163.com>
2015-12-22 16:57:22 +0800

    NavigationView

--- app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
+++ app/src/main/java/com/ysj/milkyway/activities/MainActivity.java
@@ -1,12 +1,17 @@
 package com.ysj.milkyway.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ysj.milkyway.R;
 import com.ysj.milkyway.views.adapters.DemoPagerAdapter;
@@ -14,13 +19,22 @@ import com.ysj.milkyway.views.adapters.DemoPagerAdapter;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,
+        NavigationView.OnNavigationItemSelectedListener {
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
     @Bind(R.id.sliding_tabs)
     TabLayout tabLayout;
     @Bind(R.id.vp_demo)
     ViewPager viewPager;
     @Bind(R.id.fab)
     FloatingActionButton fab;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     private DemoPagerAdapter pagerAdapter;
 
@@ -32,7 +46,11 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        setupToolbar();
+        setSupportActionBar(toolbar);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        navigationView.setNavigationItemSelectedListener(this);
 
         pagerAdapter = new DemoPagerAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(pagerAdapter);
@@ -56,9 +74,39 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         viewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
     }
 
-    private void setupToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                drawerLayout.openDrawer(GravityCompat.START);
+//                return true;
+//        }
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        drawerLayout.closeDrawers();
+        return true;
     }
 
     private void updateTabs() {
